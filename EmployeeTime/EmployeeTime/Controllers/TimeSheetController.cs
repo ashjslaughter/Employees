7233f93a579391212db4520e66a9b376de8fba11
Ashley Slaughter <ashley.j.slaughter@gmail.com>
2014-07-18 09:39:06 -0400

    Created TimeSheet Controller

---
+++ EmployeeTime/EmployeeTime/Controllers/TimeSheetController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeTime.Models;
+
+namespace EmployeeTime.Controllers
+{
+    public class TimeSheetController : Controller
+    {
+        private EmployeeContext db = new EmployeeContext();
+
+        // GET: TimeSheet
+        public ActionResult Index()
+        {
+            return View(db.TimeSheets.ToList());
+        }
+
+        // GET: TimeSheet/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TimeSheet timeSheet = db.TimeSheets.Find(id);
+            if (timeSheet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(timeSheet);
+        }
+
+        // GET: TimeSheet/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TimeSheet/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TimeSheetId,Date,TimeIn,TimeOut,DailyHours")] TimeSheet timeSheet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TimeSheets.Add(timeSheet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(timeSheet);
+        }
+
+        // GET: TimeSheet/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TimeSheet timeSheet = db.TimeSheets.Find(id);
+            if (timeSheet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(timeSheet);
+        }
+
+        // POST: TimeSheet/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TimeSheetId,Date,TimeIn,TimeOut,DailyHours")] TimeSheet timeSheet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(timeSheet).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(timeSheet);
+        }
+
+        // GET: TimeSheet/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TimeSheet timeSheet = db.TimeSheets.Find(id);
+            if (timeSheet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(timeSheet);
+        }
+
+        // POST: TimeSheet/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TimeSheet timeSheet = db.TimeSheets.Find(id);
+            db.TimeSheets.Remove(timeSheet);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
